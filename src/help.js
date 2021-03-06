b5a5269b7319fd70da96b1cfb73143207f70af3d
Guga0706p <77945834+Guga0706p@users.noreply.github.com>
2021-03-06 13:08:14 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,150 @@
+const //database
+	return `
+╔══✪〘 INFO 〙✪══
+║
+╠➥ EREN BOT
+╠➥ 𝐃𝐎𝐍𝐎:  ⃬⃗Ëŕën
+╠➥ 𝐒𝐓𝐀𝐓𝐔𝐒: ON
+║
+╠══✪〘 NOVIDADES 〙✪══
+║
+║*${prefix}gay [@]*
+║*${prefix}wame*
+║*${prefix}spamcall*
+║*${prefix}play (nome da msc)*
+║
+╠══✪〘 MENU 〙✪══
+║
+║ *${prefix}figu*
+║ *${prefix}toimg*
+║ *${prefix}meme*
+║ *${prefix}memeindo*
+║ *${prefix}tts*
+║ *${prefix}lolih [on]*
+║ *${prefix}nsfwloli [off]*
+║ *${prefix}url2img*
+║ *${prefix}leens [na legenda]*
+║ *${prefix}wait [na legenda]*
+║ *${prefix}setprefix*
+║
+╠══✪〘 OUTROS 〙✪══
+║
+║ *${prefix}linkgp*
+║ *${prefix}simih [1/0]*
+║ *${prefix}marcar*
+║ *${prefix}add [@]*
+║ *${prefix}banir [@]*
+║ *${prefix}promover [@]*
+║ *${prefix}rebaixar*
+║ *${prefix}admins*
+║ *${prefix}marcar2*
+║ *${prefix}bc [texto]* (ele faz uma ™)
+║ *${prefix}marcar3*
+║ *${prefix}bloqueados*
+║ *${prefix}bloquear [@]*
+║ *${prefix}desbloquear [@]*
+║ *${prefix}limpar*
+║ *${prefix}bc [ *texto* ]*
+║ *${prefix}bemvindo [1/0]*
+║ *${prefix}clonar [@]*
+║ *${prefix}owner*
+║ *${prefix}tts [texto]*
+║ *${prefix}setnome*
+║ *${prefix}setfoto*
+║ *${prefix}grupoinfo*
+║ *${prefix}ytmp4*
+║ *${prefix}bomdia*
+║ *${prefix}boanoite*
+║ *${prefix}marcar*
+║ *${prefix}marcar2*
+║ *${prefix}marcar3*
+║
+╠══✪〘 IMAGENS 〙✪══
+║
+║ *${prefix}loli* [off]
+║ *${prefix}loli1*
+║ *${prefix}hentai*
+║ *${prefix}porno*
+║ *${prefix}boanoite*
+║ *${prefix}bomdia*
+║ *${prefix}boatarde*
+║ *${prefix}mia*
+║ *${prefix}mia1*
+║ *${prefix}mia2*
+║ *${prefix}belle*
+║ *${prefix}belle1*
+║ *${prefix}belle2*
+║ *${prefix}belle3*
+║ *${prefix}akeno*
+║ *${prefix}meme*   
+║ *${prefix}lofi*
+║ *${prefix}malkova*
+║ *${prefix}canal*
+║ *${prefix}nsfwloli1*
+║ *${prefix}reislin*
+║
+╠══✪〘 INTELIGÊNCIA IA 〙✪══
+║
+║ *${prefix}simih 1 (para ativar)*
+║ *${prefix}simih 0 (para desativar)*
+║ *${prefix}simi (sua mensagem)*
+║
+║
+╠══✪〘 GRUPO 〙✪══
+║
+║ *${prefix}banir*
+║ *${prefix}leveling [on/off]*
+║ *${prefix}level*
+║ *${prefix}add*
+║ *${prefix}promover*
+║ *${prefix}setfoto [na legenda]*
+║ *${prefix}setname [texto]*
+║ *${prefix}rebaixar*
+║ *${prefix}admins*
+║ *${prefix}marcar*
+║ *${prefix}marcar2*
+║ *${prefix}marcar3*
+║ *${prefix}bemvindo [1/0]*
+║ *${prefix}grupoinfo*
+║ *${prefix}bomdia*
+║ *${prefix}boatarde*
+║ *${prefix}boanoite*
+║ *${prefix}setdesc*
+║ *${prefix}bug [sua mensagem]*
+║
+╠══✪〘 ESPECIFICO DO BOT 〙✪══
+║
+║ *${prefix}bug [sua mensagem]*
+║ *${prefix}clonar [@]*
+║ *${prefix}dono*
+║ *${prefix}ping [ver velocidade do bot]*
+║ *${prefix}termux*
+║ *${prefix}gay [@]*
+║ *${prefix}wame*
+║ *${prefix}map (nome)*
+║ *${prefix}setppbot (marque uma img)*
+║ *${prefix}pinterest (nome)*
+║ *${prefix}desligar (so para o dono)*
+║ *${prefix}timer*
+║
+╠══✪〘 MAIS ALGUNS 〙✪══
+║
+║ *${prefix}neko*
+║ *${prefix}ttp [texto]*
+║ *${prefix}testime*
+║ *${prefix}tomp3*
+║ *${prefix}animehug*
+║ *${prefix}shoturl*
+║ *${prefix}pokemon*
+║
+║
+║
+║ 
+║
+╚═〘 👻 EREN BOT 👻 〙`
+}
+
+//Online.database
+
+
+
